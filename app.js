e9121d9895da8330ece6231f6ef8e1b5b6783bf2
Nitesh9609 <sharmanitesh920@gmail.com>
2024-02-26 23:13:37 +0530

    cors fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = new createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://basic-chat-fe.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true
     }
